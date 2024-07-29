49244dcbcb136b9f9ed765d77e1352418e8713d2
Shruthi5ch <138290030+Shruthi5ch@users.noreply.github.com>
2024-07-30 00:26:55 +0530

    Add files via upload

---
+++ Port_Scan.py
@@ -0,0 +1,18 @@
+
+!apt-get update
+!pip install python-nmap
+!apt-get install nmap
+!which nmap
+import nmap
+nm=nmap.PortScanner()
+target = '45.33.32.156'
+options = '-sV -sC scan_results'
+nm.scan(target, arguments=options)
+for host in nm.all_hosts():
+    print('Host: %s (%s)' % (host,nm[host].hostname()))
+    print('state:%s' % nm[host].state())
+    for protocol in nm[host].all_protocols():
+        print('protocol:%s' % protocol)
+        port_info= nm[host][protocol]
+        for port,state in port_info.items():
+            print('Port: %s\t State: %s' % (port,state))
